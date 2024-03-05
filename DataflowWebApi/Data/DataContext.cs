67d329cd5c741fa3958082374d1261bf98848ce9
Nelson Pino <devsoftware.etiquetas@gmail.com>
2024-03-05 10:56:32 -0400

    creacion de la base de datos en sql server con entityframework core

---
+++ DataflowWebApi/Data/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using DataflowWebApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataflowWebApi.Data
+{
+	public class DataContext: DbContext
+	{
+        public DataContext(DbContextOptions<DataContext> options) : base(options) 
+        {
+        }
+        public DbSet<Products> Products { get; set; }
+    }
+}
---
+++ DataflowWebApi/Models/Products.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DataflowWebApi.Models
+{
+	public class Products
+	{
+		[Key]
+		public int ProductId { get; set; }
+		public string ProductName { get; set; } = string.Empty;
+		public string ProdcutCategory { get; set; } = string.Empty;
+        public double ProductPrice { get; set; }
+		public string CodeBar { get; set; } = string.Empty;
+	}
+}
